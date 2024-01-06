24bedae721017281cbac344a7b5b932d2714ac4d
iwanms <iwanmaolanasidik1@gmail.com>
2024-01-06 22:42:57 +0700

    video 6

--- app/src/main/java/com/example/latihan/MainActivity.kt
+++ app/src/main/java/com/example/latihan/MainActivity.kt
@@ -5,21 +5,23 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val etName = findViewById<EditText>(R.id.etName)
-        val btnSay = findViewById<Button>(R.id.btnSay)
-        val etHello = findViewById<EditText>(R.id.etHello)
+        val nilai_a = findViewById<EditText>(R.id.etNilaiA)
+        val nilai_b = findViewById<EditText>(R.id.etNilaiB)
+        val btnJml = findViewById<Button>(R.id.btnJml)
+        val tvHasil = findViewById<TextView>(R.id.tvHasil)
 
-        btnSay.setOnClickListener{
-            val name = etName.text.toString()
-            etHello.setText("Hello $name, kcawwww")
-
-            Log.d("Name : ", "Hello $name")
+        btnJml.setOnClickListener{
+            val n_a = nilai_a.text.toString().toInt()
+            val n_b = nilai_b.text.toString().toInt()
+            val n_jml = n_a + n_b
+            tvHasil.text = n_jml.toString()
         }
     }
 }
